f70b443d1149e61f82291aa946e7fd94265a9104
vivmagarwal <vivmagarwal@gmail.com>
2022-11-08 22:40:59 +0530

    feat: Recipe added.

--- generateThumbnails.js
+++ generateThumbnails.js
@@ -15,7 +15,9 @@ import path from "node:path";
 dotenv.config();
 
 // just change base path; then run node generateThumbnails.js
-const basePath = "public/images/recipe-ingredient";
+const basePath = "public/images-raw/recipe-ingredient";
+const toPath = "public/images/recipe-ingredient"
+const optimizeSelf = true;
 
 const files = await readdir(basePath);
 for (const file of files) {
@@ -23,11 +25,33 @@ for (const file of files) {
   let perhapsThumbPath =
     basePath + "/" + path.parse(file).name + "-thumb" + path.parse(file).ext;
 
+  // optimize self
+  if (optimizeSelf && !path.parse(file).name.endsWith('thumb')) {
+    sharp(filePath)
+      .resize(350)
+      .png(70)      
+      .toFile(
+        toPath +
+          "/" +
+          path.parse(file).name +
+          path.parse(file).ext,
+        (err, resizeImage) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(resizeImage + " " + filePath);
+          }
+        }
+      );
+  }
+
+  // generate thumb
   if (!path.parse(file).name.endsWith('thumb') && !existsSync(perhapsThumbPath)) {
     sharp(filePath)
-      .resize(200, 200)
+      .resize(100, 100)
+      .png(70) 
       .toFile(
-        basePath +
+        toPath +
           "/" +
           path.parse(file).name +
           "-thumb" +
